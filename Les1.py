8b2450984d0016c0bc06a3dd8c38dd46c7993e7f
SovaIna <innasavenko4@icloud.com>
2022-09-26 22:04:10 +0300

    create file less1

---
+++ Les1.py
@@ -0,0 +1,22 @@
+peremenA = 11 #int
+peremenB = 67678 #int
+print (peremenB,peremenA)
+peremenC = 23.45 #float
+peremenD = 1.987 #float
+print (peremenC,peremenD)
+peremenE = "Python syper" #str
+peremenF = "Hi,teachers" #str
+print (peremenE,peremenF)
+iunderstand = False
+print (type(iunderstand))
+understoodnothing = True
+print (type(understoodnothing))
+peremenI = None #пустой тип
+print(peremenI)
+
+
+
+
+
+
+
